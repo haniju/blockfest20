071e74d44f09da49fc6dcad27a456c0385db672d
Pierre-Alexis Ciavaldini <pierre-alexis@ciavaldini.fr>
2017-04-06 18:55:58 +0200

    dynamic colors

--- blockfest.js
+++ blockfest.js
@@ -1,2 +1,18 @@
-angular.module("blockfest", []).controller("main", function () {
-});
+angular.module("blockfest", []);
+
+angular.module("blockfest").controller
+("main", [ "$scope", function ($scope) {
+
+	var tones  = [ "dark", "bright" ];
+	var colors = [
+		"mint",
+		"lemon",
+		"raspberry",
+		"green",
+		"night",
+		"violet"
+	];
+
+	$scope.color = colors[Math.floor(Math.random() * colors.length)];
+	$scope.tone = tones[Math.random() > 0.5 ? 0 : 1];
+} ]);
